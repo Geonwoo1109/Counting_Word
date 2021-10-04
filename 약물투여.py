3ff62a459b544c0b3f68be830d5a24eab54ec26a
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2021-10-04 23:53:56 +0900

    Update 약물투여.py

--- 약물투여.py
+++ 약물투여.py
@@ -1,5 +1,21 @@
 while(1):
+  p = input("p: ")
+  if (p == "a" or p == "k"):
+    print("\n")
+  else:
+    print("입력 값이 잘못되었습니다.")
+    continue
+    
+  w = input("w: ")
   
+  if (p == "a"):
+    a = w * 0.5
+  else:
+    a = w * 0.3
+    
+  a (p, w)
+  
+  break
   
 def a (p, w):
-  print()
+  print("환자가 " + p + "인 사람에게 " + a + "만큼 투여함")
